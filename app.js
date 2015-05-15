ad696662266b77c04f288a2c9a0f2a5143a16b61
Tomas Batalla <tebatalla@gmail.com>
2015-05-15 11:59:42 -0700

    Directive to attach popover conditionally

--- app.js
+++ app.js
@@ -26,9 +26,52 @@ angular.module('demo', [])
         $http.get(owner.followers_url).
         success(function(data, status, headers, config) {
           angular.forEach(data, function (follower){
-            owner.followers.push(follower);
+            owner.followers.push(follower.login);
           });
         });
+      }
+    };
+  }])
+  .directive('hoverable', [function() {
+    return {
+      scope: {
+        canHover: '&',
+        owner: '='
+      },
+
+      link: function(scope, element, attr) {
+        
+        if(scope.canHover()){
+          attachPopover(scope, element);
+        }
+
+        function attachPopover(scope, element) {
+          if(scope.popover) {
+            return;
+          } else {
+            $(element).popover({
+              animation: true,
+              content: followers,
+              placement: 'auto left',
+              trigger: 'hover',
+              html: true,
+              title: title
+            });
+            scope.popover = true;
+          }
+        }
+
+        function title() {
+          return scope.owner.login + "'s followers";
+        }
+
+        function followers() {
+          var $group = $("<ul class='followers'>");
+          scope.owner.followers.forEach(function(follower) {
+            $group.append("<li>"+follower+"</li>");
+          });
+          return $group;
+        }
       }
     };
   }]);
